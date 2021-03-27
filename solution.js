d0244d931804c9a04d55dbce19b8369fa5134431
Lidia Godo <lidiagodo@MacBook-Pro-Lidia.local>
2021-03-27 11:02:27 +0300

    added functional for collecting all lines of puzzle

--- solution.js
+++ solution.js
@@ -1,16 +1,12 @@
 function solvePuzzle(pieces) {
   const readyPieces = [];
   
-
 //функция проверки переданного условия
 function checkConditions(obj, keysValues) {
   if (!keysValues.length) return false;
 
   let result = keysValues
       .map(keyValue => {
-        // console.log('obj ', obj);
-        // console.log('key value ', equalObject(obj[`${keyValue[0]}`]), equalObject(keyValue[1]));
-        // console.log('key = value', equalObject(obj[`${keyValue[0]}`]) === equalObject(keyValue[1]));
         return equalObject(obj[`${keyValue[0]}`]) === equalObject(keyValue[1]);
       })
       .reduce((total, current) => 
@@ -65,30 +61,16 @@ function equalObject(obj) {
   return JSON.stringify(Object.fromEntries(Object.entries(obj).sort()));
 };
 
-//функция поиска подxодящего к этой грани куска (и поворота найденного куска?????)
+//функция поиска подxодящего к этой грани куска 
 function findPiece(piecesArray, suitableEdge) {
-  // function findPiece(piecesArray, suitableEdge, sideShouldBe) {
-console.log('suitableEdge in findPiece = ', suitableEdge);
-
   return piecesArray.find(elem => {
-    console.log('elem.edges = ', elem.edges);
     for (let edge in elem.edges) {
 
       const edgeObj = elem.edges[`${edge}`];
       if (edgeObj === null) {
-// console.log('edgeObj null');
         continue;
       }
-
-//       if (edgeObj.type === suitableEdge.type) {
-// console.log('edgeObj', edgeObj);
-// console.log('suitableEdge', suitableEdge);
-//       }
-
       if (equalObject(edgeObj) === equalObject(suitableEdge)) {
-        // console.log('edge должна стать левой', edge);
-        // elem.edges = spinElement(elem.edges, {[`${sideShouldBe}`]: edgeObj});//какой должна стать найденная сторона - left, top, right, bottom
-
         return true;
       }
     }
@@ -96,45 +78,39 @@ console.log('suitableEdge in findPiece = ', suitableEdge);
 };
 
 let suitableTopEdge = {};
-
+for (let row = 1; row <= 10; row++) {
   let currentElement = {};
-
-
+  if (row === 1) {
     currentElement = {...pieces[0]};
     currentElement.edges = spinElement(currentElement.edges, {'left': null, 'top': null});
-
-
+  } else {
+    currentElement = findPiece(pieces, suitableTopEdge);
+    currentElement.edges = spinElement(currentElement.edges, {'top': suitableTopEdge});
+  }
   
   readyPieces.push([currentElement.id]);
   pieces = pieces.filter(piece => piece.id !== currentElement.id);
-console.log('первый элемент = ', currentElement);
-console.log('pieces.length =', pieces.length);
 
-  // if () {
+  if (row<10) {
     suitableTopEdge = {...currentElement.edges.bottom};//запоминаем положение нижней грани первого в линии элемента
     toggleIO(suitableTopEdge);
-    console.log('suitableTopEdge = ', suitableTopEdge);
-  // }
+  }
 
     let suitableLeftEdge = {...currentElement.edges.right};//следующий подходящий элемент
     
-    for (let col=1; col<10; col++) {//заполняем первую строчку
+    for (let col=1; col<10; col++) {//заполняем строчку
       toggleIO(suitableLeftEdge);
-// console.log('ищем suitableLeftEdge = ', suitableLeftEdge);
 
       const nextElement = {...findPiece(pieces, suitableLeftEdge)};
+      nextElement.edges = spinElement(nextElement.edges, {'left': suitableLeftEdge});
 
-nextElement.edges = spinElement(nextElement.edges, {'left': suitableLeftEdge});
-
-// console.log('будем искать грань для nextElement.rigth = ', nextElement.edges.right);// если это не последний элемент в линии
-      
       readyPieces.push([nextElement.id]);
       pieces= pieces.filter(piece => piece.id !== nextElement.id);
 
       suitableLeftEdge = nextElement.edges.right;
     }
 
-  
+  }
   return readyPieces;
 }
 
